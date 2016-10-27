858cdef92086e7cd5abf68da8e383f60c621582b
benkick <benkick@VF-070065N.intra.dlr.de>
2016-10-27 13:58:56 +0200

    worked on the secondary car market

--- asdf/main/java/org/fleet/core/FleetModeling.java
+++ asdf/main/java/org/fleet/core/FleetModeling.java
@@ -26,10 +26,11 @@ public class FleetModeling {
 	private final Random random;
 	private final int noOfVeh;
 	private final int noOfHH;
+	private int baseYear;
 	
 	private final Vehicles vehicles;
 	private final List<Id<Vehicle>> assignedVeh;
-	private List<Id<Vehicle>> leftVeh;
+	private final List<Id<Vehicle>> remainingVeh;
 	private final Households households;
 	//TODO: How to treat company-owned, but (mainly) privately-used vehicles?
 	//private Companies companies;
@@ -37,14 +38,17 @@ public class FleetModeling {
 	private final HouseholdUtils hhUtils;
 	private final VehicleUtils vehUtils;
 
-	public FleetModeling(Random random, int noofveh, int noofhh){
+
+	public FleetModeling(Random random, int noofveh, int noofhh, int baseyear){
 		this.random = random;
 		this.noOfVeh = noofveh;
 		this.noOfHH = noofhh;
+		this.baseYear = baseyear;
 		
 		this.vehicles = new Vehicles();
+		//TODO: is assignedVeh really needed?
 		this.assignedVeh = new ArrayList<Id<Vehicle>>();
-		this.leftVeh = new ArrayList<Id<Vehicle>>();
+		this.remainingVeh = new ArrayList<Id<Vehicle>>();
 		this.households = new Households();
 		this.hhUtils = new HouseholdUtils();
 		this.vehUtils = new VehicleUtils();
@@ -54,19 +58,24 @@ public class FleetModeling {
 		log.info("Entering preprocessing...");
 		generateInitialVehicles();
 		generateInitialHouseholds();
-		writeSecondHandCarMarket();
 		log.info("Leaving preprocessing...");
 	}
 
-	public void run() {
+	public void run(int iterations) {
 		log.info("Entering simulation...");
-//		scrapeVehicles();
-		//The following could be influenced by many factors, e.g. how many vehicles die
-		//TODO: Benjamin
-//		generateNewVehicles();
-//		assignNewVehicles2HH();
-		//TODO: Marie
-//		modelSecondHandCarMarket();
+		for(int i=0; i<iterations; i++){
+			log.info("=================================================");
+			log.info("Simulating transactions for year " + this.baseYear);
+			log.info("=================================================");
+//			scrapeVehicles();
+			//The following could be influenced by many factors, e.g. how many vehicles die
+			//TODO: Benjamin
+//			generateNewVehicles();
+//			assignNewVehicles2HH();
+			//TODO: Marie
+			modelSecondHandCarMarket();
+			this.baseYear += 1;
+		}
 		log.info("Leaving simulation...");
 	}
 
@@ -77,6 +86,66 @@ public class FleetModeling {
 		log.info("Shutting down.");
 	}
 	
+	private void modelSecondHandCarMarket() {
+		Vehicles vehForSale = chooseVehiclesForSale();
+		log.info("Cars for sale in the secondhand car market: "+ vehForSale.getVehicles().size());
+		
+		Households buyingHH = chooseBuyingHHs();
+		log.info("Households buying used cars: "+ buyingHH.getHouseholds().size());
+		
+		clearSecondHandMarket();
+		
+		
+	}
+
+	private void clearSecondHandMarket() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private Vehicles chooseVehiclesForSale(){
+		Vehicles vehForSale = new Vehicles();
+		for(Id<Vehicle> vid : this.assignedVeh){
+			Vehicle veh = this.vehicles.getVehicles().get(vid);
+			if(markVehForSale(veh)){
+				vehForSale.addVehicle(veh);
+			}
+		}
+		return vehForSale;
+	}
+
+	private boolean markVehForSale(Vehicle veh){
+		double rand = random.nextDouble();
+		Drivetrain drivetrain = veh.getDt();
+		boolean sellVeh = false;
+
+		if(drivetrain.equals(Drivetrain.GASOLINE)){
+			if(rand < 0.1645) sellVeh = true;
+		}else if(drivetrain.equals(Drivetrain.DIESEL)){
+			if(rand < 0.1665) sellVeh = true;
+		}else if(drivetrain.equals(Drivetrain.NATURAL_GAS)){
+			if(rand < 0.1687) sellVeh = true;
+		}else if(drivetrain.equals(Drivetrain.HYBRID)){
+			if(rand < 0.1361) sellVeh = true;
+		}else if(drivetrain.equals(Drivetrain.BEV)){
+			if(rand < 0.1266) sellVeh = true;
+		}else{
+			throw new RuntimeException("No transaction probability defined for drivetrain " + drivetrain + ". Aborting...");
+		}
+		return sellVeh;
+	}
+
+	private Households chooseBuyingHHs(){
+		Households buyingHHs = new Households();
+		for(Household hh : this.households.getHouseholds().values()){
+			double rd = random.nextDouble();
+			if(rd<0.2){
+				buyingHHs.addHousehold(hh);
+			}
+		}
+		return buyingHHs;
+	}
+
 	private void writeInitialVehicleInformation() {
 		vehUtils.countDt2Vehicles(this.vehicles);
 		for(Drivetrain dt : vehUtils.getDtCount().keySet()){
@@ -89,7 +158,7 @@ public class FleetModeling {
 		for(Integer vehInHHClass : hhUtils.getNoOfVeh2NoOfHHCount().keySet()){
 			log.info("Households with " + vehInHHClass + " vehicle(s): " + hhUtils.getNoOfVeh2NoOfHHCount().get(vehInHHClass) + " of " + households.getHouseholds().size() + " households.");
 		}
-		log.info("Vehicles assigned to households: " + hhUtils.getTotalVehInAllHHCnt() +  "(check sum: " + this.assignedVeh.size() + ").");
+		log.info("Vehicles assigned to households: " + hhUtils.getTotalVehInAllHHCnt() +  " (check sum: " + this.assignedVeh.size() + ").");
 		int marketMismatchCnt = vehUtils.getTotalVehCnt() - hhUtils.getTotalVehInAllHHCnt();
 		log.info("Vehicles not assigned to households: " + marketMismatchCnt); //is always >= 0 because otherwise some HH would not have gotten enough vehicles
 	}
@@ -101,7 +170,7 @@ public class FleetModeling {
 			Drivetrain dt = determineDrivetrain();
 			Vehicle veh = new Vehicle(vid, dt);
 			this.vehicles.addVehicle(veh);
-			this.leftVeh.add(veh.getId());
+			this.remainingVeh.add(veh.getId());
 		}
 		writeInitialVehicleInformation();
 		log.info("Leaving initial vehicle generation...");
@@ -127,41 +196,16 @@ public class FleetModeling {
 		}
 	}
 
-	//TODO: it might happen that there are more/less vehicles than needed...
-	//TODO: zufälliges Ziehen ohne zurücklegen > performanter machen?
-//	private Vehicle chooseVehicle() {
-//		Vehicle chosenVeh = null;
-//		Map<Id<Vehicle>, Vehicle> availVeh = this.vehicles.getVehicles();
-//		if(this.assignedVeh.size()>=availVeh.size()){
-//			throw new RuntimeException("All vehicles have already been assigned to households. Aborting...");
-//		}
-//		
-//		for(int i=0; i<availVeh.size(); i++){
-//			int rd = random.nextInt(availVeh.size()) + 1;
-//			log.info("Random " +rd);
-//			chosenVeh = availVeh.get(Id.createVehicleId(rd));
-//			log.info("Chosen veh " + chosenVeh);
-//			if(!this.assignedVeh.contains(chosenVeh.getId())){
-//				this.assignedVeh.add(chosenVeh.getId());
-//				return chosenVeh;
-//			} else { 
-//				//chosen vehicle already assigned, go to next random vehicle
-//			}
-//		}
-//		return chosenVeh;
-//	}
-
-// new chooseVehicle method, runs without errors, but does it do what we want? Marie, october 21th 
 	private Vehicle chooseVehicle(){
 		Id<Vehicle> chosenVehId = null;
 		Vehicle chosenVeh = null;
-		if(this.leftVeh.size()==0){
+		if(this.remainingVeh.size()==0){
 			throw new RuntimeException("All vehicles have already been assigned to households. Aborting...");
 		}
-		int rd = random.nextInt(this.leftVeh.size()); 
-		chosenVehId = leftVeh.get(rd);
+		int rd = random.nextInt(this.remainingVeh.size()); 
+		chosenVehId = remainingVeh.get(rd);
 		chosenVeh = vehicles.getVehicles().get(chosenVehId);
-		this.leftVeh.remove(chosenVehId);
+		this.remainingVeh.remove(chosenVehId);
 		this.assignedVeh.add(chosenVehId);
 		return chosenVeh;
 	}
@@ -178,7 +222,6 @@ public class FleetModeling {
 		return noOfCars;
 	}
 
-	//TODO: not all drivetrains considered!
 	private Drivetrain determineDrivetrain() {
 		Drivetrain dt;
 		double rd = random.nextDouble();
@@ -200,52 +243,4 @@ public class FleetModeling {
 	//	Vehicles vehForSale = chooseVehForSale();
 	//	Households buyingHH = chooseBuyingHH();
 	//}
-	
-	//TODO: do not iterate over vehicles - iterate over assignedVeh
-	private Vehicles chooseVehForSale(){
-		Vehicles vehForSale = new Vehicles();
-		ArrayList <Id<Vehicle>> vehIdForSale = new ArrayList<Id<Vehicle>>();
-		for(Map.Entry<Id<Vehicle>,Vehicle> entry : this.vehicles.getVehicles().entrySet()){
-			double rd = random.nextDouble();
-			if(sellUsedVeh(entry.getValue(),rd)){
-				vehForSale.getVehicles().put(entry.getKey(), entry.getValue()) ; 
-			}	
-		}
-		return vehForSale;
-	}
-	
-	private boolean sellUsedVeh(Vehicle veh, Double rand){
-		Drivetrain drivetrain = veh.getDt();
-		if(drivetrain.equals(Drivetrain.GASOLINE) && rand < 0.1645){
-			return true;
-		}else if(drivetrain.equals(Drivetrain.DIESEL) && rand < 0.1665){
-			return true;
-		}else if(drivetrain.equals(Drivetrain.NATURAL_GAS) && rand < 0.1687){
-			return true;
-		}else if(drivetrain.equals(Drivetrain.HYBRID) && rand < 0.1361){
-			return true;
-		}else if(drivetrain.equals(Drivetrain.HYBRID) && rand < 0.1266){
-			return true;
-		}else{
-			return false;
-		}
-	}
-	private void writeSecondHandCarMarket(){
-		Vehicles example = chooseVehForSale();
-		log.info("Cars for sale in the secondhand car market: "+ example.getVehicles().size());
-		Households buyingHH = chooseBuyingHH();
-		log.info("Households buying used cars: "+ buyingHH.getHouseholds().size());
-		
-	}
-	
-	private Households chooseBuyingHH(){
-		Households buyingHH = new Households();
-		for(Map.Entry<Id<Household>,Household> entry : this.households.getHouseholds().entrySet()){
-			double rd = random.nextDouble();
-			if(rd<0.2){
-				buyingHH.getHouseholds().put(entry.getKey(), entry.getValue());
-			}
-		}
-		return buyingHH;
-	}
 }
--- asdf/main/java/org/fleet/run/RunFleetModeling.java
+++ asdf/main/java/org/fleet/run/RunFleetModeling.java
@@ -13,15 +13,18 @@ public class RunFleetModeling {
 	private final static Random random = new Random(1331);
 	private final static int noOfVeh = 12000;
 	private final static int noOfHH = 10000;
+	private final static int baseYear = 2016;
+	private final static int noOfIterations = 1;
 
 	public static void main(String[] args) {
 		FleetModeling fleetModeling = new FleetModeling(
 				random,
 				noOfVeh,
-				noOfHH
+				noOfHH,
+				baseYear
 				);
 		fleetModeling.preprocess();
-		fleetModeling.run();
+		fleetModeling.run(noOfIterations);
 		fleetModeling.postprocess();
 	}
 }
